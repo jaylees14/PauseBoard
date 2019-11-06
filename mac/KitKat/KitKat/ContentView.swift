53dcddeaf825cde85a07ce0b8550bd3dcca521e7
Jay Lees <jay.lees@me.com>
2019-11-06 14:47:22 +0000

    Add base Mac app

---
+++ mac/KitKat/KitKat/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  KitKat
+//
+//  Created by Jay Lees on 06/11/2019.
+//  Copyright © 2019 Team Macro. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
