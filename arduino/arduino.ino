fec7c519919c3dbeeb8cfdfe2fefaa5f192503aa
Ben <benlee98@live.com>
2019-11-13 12:39:00 +0000

    Implement send data and debug echo

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,4 +1,6 @@
-void setup() { Serial.begin(9600); }
+void setup() {
+  Serial.begin(9600);
+}
 
 bool shift = false;
 
@@ -6,7 +8,21 @@ char chars[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15};
 
 void loop() {
   delay(1000);
+  int data = receiveData();
+  if (data != -1) {
+    sendData(data, false);
+  }
+}
+
+void sendData(char c, bool shift) {
   Serial.write(0x80 | (shift ? 0b1 : 0b0));
-  Serial.write(chars[rand() % sizeof(chars)]);
+  Serial.write(c);
   Serial.write(0);
+}
+
+int receiveData() {
+  if (Serial.available() > 0) {
+    return Serial.read();
+  }
+  return -1;
 }
