cdec1fdb96e8ba4c39a01d499b71b3981467665f
Charlie Harding <charlie_harding@icloud.com>
2019-11-27 22:28:07 +0000

    Add handling of the keys

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,22 +1,48 @@
+const size_t KEYS = 4;
+
+char pins[KEYS] = {A0, A1, A2, A3};
+
 void setup() {
   Serial.begin(9600);
+  for (int i = 0; i < KEYS; i++) {
+    pinMode(pins[i], INPUT);
+  }
 }
 
 bool shift = false;
 
-char chars[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15};
+const int threshold = 200;
+const long repeat = 300;
+const long firstRepeatDelay = 300;
+char chars[KEYS] = {'A', 'B', 'C', 'D'};
+long lastPressed[KEYS] = {0};
 
 void loop() {
-  delay(1000);
-  int data = receiveData();
-  if (data != -1) {
-    sendData(data, false);
+  delay(100);
+  // int data = receiveData();
+  for (int i = 0; i < KEYS; i++)
+    handleKey(i);
+}
+
+void handleKey(int i) {
+  if (analogRead(pins[i]) > threshold) {
+    if (lastPressed[i] == 0) {
+      lastPressed[i] = millis() + firstRepeatDelay;
+    } else if (lastPressed[i] > millis() + repeat) {
+      return;
+    } else {
+      lastPressed[i] = millis();
+    }
+    // Serial.println(chars[i]);
+    sendData(i, false);
+  } else {
+    lastPressed[i] = 0;
   }
 }
 
 void sendData(char c, bool shift) {
   Serial.write(0x80 | (shift ? 0b1 : 0b0));
-  Serial.write(c);
+  Serial.write(c + 1);
   Serial.write(0);
 }
 
